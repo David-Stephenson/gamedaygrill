e362c6d7e07480e67ff09cdeb88e86be250bdbe2
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-11-19 17:41:34 -0500

    Improvements
    - Add +.svlete-kit to gitignore
    - Improve footer
    - Change height of hero
    - Add rewards banner on landing page

--- src/components/global/Footer.svelte
+++ src/components/global/Footer.svelte
@@ -1,30 +1,35 @@
 <script>
-	import { Facebook, Twitter, Instagram } from 'lucide-svelte';
-	import Logo from '$components/global/Logo.svelte';
+  import { Facebook, Twitter, Instagram } from "lucide-svelte";
+  import Logo from "$components/global/Logo.svelte";
 
-	const socialLinks = {
-		Facebook: { component: Facebook, url: 'https://www.facebook.com' },
-		Twitter: { component: Twitter, url: 'https://www.twitter.com' },
-		Instagram: { component: Instagram, url: 'https://www.instagram.com' }
-	};
+  const socialLinks = {
+    Facebook: { component: Facebook, url: "https://www.facebook.com" },
+    Twitter: { component: Twitter, url: "https://www.twitter.com" },
+    Instagram: { component: Instagram, url: "https://www.instagram.com" },
+  };
 </script>
 
-<footer class="bg-gray-800 text-white py-6 px-4">
-	<div class="container mx-auto flex flex-wrap justify-between items-center">
-		<div
-			class="w-full md:w-auto mb-4 md:mb-0 text-center md:text-left flex flex-col md:flex-row items-center"
-		>
-			<Logo class="h-12 fill-white mr-8" />
-			<div>
-				<h5 class="text-lg font-russo">Game Day Grill</h5>
-			</div>
-		</div>
-		<div class="flex justify-center md:justify-start">
-			{#each Object.entries(socialLinks) as [name, link]}
-				<a href={link.url} class="text-white hover:text-gray-300 mx-2">
-					<svelte:component this={link.component} size={24} />
-				</a>
-			{/each}
-		</div>
-	</div>
+<footer class="bg-white text-gray-800 py-8 px-4">
+  <div class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 items-start justify-between">
+    <div class="flex items-center space-x-2 md:space-x-4">
+      <Logo class="h-12 fill-red-500" />
+      <div>
+        <h5 class="text-lg font-russo text-red-500">Game Day Grill</h5>
+        <p class="text-sm italic text-gray-600">"Come for the game, stay for the grill!"</p>
+      </div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-2 text-gray-600">
+      <p class="text-sm">38 W Bridge St, Dublin, OH</p>
+      <p class="text-sm">Phone: (614) 764-4932</p>
+      <p class="text-sm">Hours: Mon-Fri 9am-9pm, Sat-Sun 10am-10pm</p>
+      <p class="text-sm">Email: contact@gameday.com</p>
+    </div>
+    <div class="flex space-x-2 items-center md:items-start">
+      {#each Object.entries(socialLinks) as [name, link]}
+        <a href={link.url} class="text-red-500 hover:text-red-700">
+          <svelte:component this={link.component} size={24} />
+        </a>
+      {/each}
+    </div>
+  </div>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,31 @@
 <script>
-	import Hero from '$components/home/Hero.svelte';
-	import DailySpecials from '$components/home/DailySpecials.svelte';
-	import Hours from '$components/home/Hours.svelte';
+  import Hero from "$components/home/Hero.svelte";
+  import DailySpecials from "$components/home/DailySpecials.svelte";
+  import Hours from "$components/home/Hours.svelte";
+  import RewardsBanner from "$components/home/RewardsBanner.svelte";
 </script>
 
 <!-- HTML Head -->
 <svelte:head>
-	<title>Game Day Grill</title>
+  <title>Game Day Grill</title>
 </svelte:head>
 
 <div class="bg-gray-50 min-h-screen flex flex-col">
-	<!-- Hero Section -->
-	<Hero />
+  <!-- Hero Section -->
+  <Hero />
 
-	<!-- Daily Specials -->
+  <!-- Rewards Banner Section -->
+  <RewardsBanner />
 
-	<div class="flex flex-row">
-		<!-- Hours -->
-		<div class="w-fit">
-			<Hours />
-		</div>
+  <!-- Daily Specials Section -->
+  <DailySpecials />
 
-		<!-- Daily Specials -->
-		<div class="w-3/4">
-			<DailySpecials />
-		</div>
-	</div>
-
-	<!-- Reservation CTA Section -->
-	<section class="py-12 px-4 bg-red-600 text-white text-center">
-		<h2 class="text-4xl font-bold mb-8">Reserve Your Table Now</h2>
-		<p class="text-xl mb-8">
-			Experience the best game day dishes in town. Reserve your table now and enjoy the game with
-			us.
-		</p>
-		<a
-			href="/reserve"
-			class="inline-block py-4 px-8 bg-white text-red-600 text-xl font-bold rounded-lg transition duration-200 hover:bg-opacity-90"
-			>Reserve Now</a
-		>
-	</section>
+  <!-- Reservation CTA Section -->
+  <section class="py-12 px-4 bg-red-600 text-white text-center">
+    <h2 class="text-4xl font-bold mb-8">Reserve Your Table Now</h2>
+    <p class="text-xl mb-8">Experience the best game day dishes in town. Reserve your table now and enjoy the game with us.</p>
+    <a href="/reserve" class="inline-block py-4 px-8 bg-white text-red-600 text-xl font-bold rounded-lg transition duration-200 hover:bg-opacity-90"
+      >Reserve Now</a
+    >
+  </section>
 </div>
