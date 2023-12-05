c5f5578f87afb7595414afc7d910267db1901a61
Connor Cochran <ccochran06@outlook.com>
2023-12-04 23:26:45 -0500

    images and new special item

--- src/components/home/DailySpecials.svelte
+++ src/components/home/DailySpecials.svelte
@@ -2,25 +2,31 @@
 	const featuredDishes = [
 		{
 			name: 'Burger Supreme',
-			description: 'Juicy beef patty with fresh veggies and our secret sauce',
-			image: '/assets/burger.jpg'
+			description: 'Juicy beef patty with fresh veggies and our secret sauce.',
+			image: 'images/burger.jpg'
 		},
 		{
 			name: 'Chicken Wings Platter',
-			description: 'Crispy wings tossed in our signature spicy sauce',
-			image: '/assets/wings.jpg'
+			description: 'Crispy wings tossed in our signature spicy sauce.',
+			image: 'images/wings.jpg'
 		},
 		{
 			name: 'Loaded Nachos',
 			description: 'Crispy nachos topped with cheese, jalapenos, and more',
-			image: '/assets/nachos.jpg'
+			image: 'images/nachos.jpg'
+		},
+		{
+			name: 'Onion Rings',
+			description: 'Hot, fresh, and crispy onion rings served with your choice of any two sauces.',
+			image: 'images/onionrings.jpg'
 		}
+		
 	];
 </script>
 
 <!-- Featured Dishes Section -->
 <section class="py-12 px-4">
-	<h2 class="text-4xl font-bold text-center mb-10">Todays Specials</h2>
+	<h2 class="text-4xl font-bold text-center mb-10">Today's Specials</h2>
 	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
 		<!-- For each dish in array -->
 		{#each featuredDishes as dish (dish.name)}
