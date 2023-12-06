b6eb79057886670e7c7abce63ce3d30ec7a98683
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-12-05 19:40:07 -0500

    Finish Reservations
    Added a confirmation screen

--- src/routes/reserve/+page.svelte
+++ src/routes/reserve/+page.svelte
@@ -1,30 +1,56 @@
+<script>
+  import { goto } from "$app/navigation";
+
+  let partyName = "";
+  let date = "";
+  let time = "";
+
+  function handleSubmit() {
+    goto(`/reserve/complete?partyName=${partyName}&date=${date}&time=${time}`);
+  }
+</script>
+
 <section class="flex justify-center items-center py-12">
   <div class="flex flex-wrap -mx-4">
     <div class="w-full lg:w-1/2 px-4 mb-4 lg:mb-0">
       <div class="max-w-xl mx-auto py-6 px-4 bg-white rounded shadow-md">
         <h2 class="text-2xl font-bold mb-4 text-center">Reserve a Table</h2>
-        <form>
+        <form on:submit|preventDefault={handleSubmit}>
           <div class="mb-4">
-            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Party Name" type="text" />
+            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Party Name" type="text" required bind:value={partyName} />
           </div>
           <div class="grid grid-cols-2 gap-4 mb-4">
-            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Phone Number" type="tel" />
-            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Email" type="email" />
+            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Phone Number" type="tel" required />
+            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Email" type="email" required />
           </div>
           <div class="grid grid-cols-2 gap-4 mb-4">
-            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Select Date" type="date" />
-            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Select Time" type="time" />
+            <input
+              class="w-full px-3 py-2 border rounded text-gray-700"
+              placeholder="Select Date"
+              type="date"
+              min={new Date().toISOString().split("T")[0]}
+              required
+              bind:value={date}
+            />
+            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Select Time" type="time" required bind:value={time} />
+          </div>
+
+          <div class="mb-4">
+            <div class="relative">
+              <select class="appearance-none w-full px-3 py-2 border rounded text-gray-700 pr-8" required>
+                <option value="" disabled selected hidden>Select Number of Guests</option>
+                {#each Array.from({ length: 9 }, (_, i) => i + 1) as number (number)}
+                  <option value={number}>{number}</option>
+                {/each}
+                <option value="10+" disabled>10+ Please contact Game Day Grill</option>
+              </select>
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 12l-6-6h12l-6 6z" /></svg>
+              </div>
+            </div>
           </div>
           <div class="mb-4">
-            <select class="w-full px-3 py-2 border rounded text-gray-700">
-              {#each [1, 2, 3, 4, 5, 6, 7, 8, 9] as number (number)}
-                <option value={number}>{number}</option>
-              {/each}
-              <option value="10+" disabled>10+ | Please contact Game Day Grill</option>
-            </select>
-          </div>
-          <div class="mb-4">
-            <input class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Special Requests" type="text" />
+            <textarea class="w-full px-3 py-2 border rounded text-gray-700" placeholder="Special Requests" rows="3"></textarea>
           </div>
           <div class="space-y-4 mb-4">
             <div class="flex items-center">
---
+++ src/routes/reserve/complete/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from "svelte";
+  let partyName = "";
+  let date = "";
+  let time = "";
+  let reservationId = "";
+
+  onMount(() => {
+    // Get the reservation details from the URL parameters or local storage
+    const urlParams = new URLSearchParams(window.location.search);
+    partyName = urlParams.get("partyName");
+    date = urlParams.get("date");
+    time = urlParams.get("time");
+    // Generate a random reservation ID
+    reservationId = Math.floor(Math.random() * 1000000);
+  });
+</script>
+
+<section class="flex justify-center items-center py-12">
+  <div class="max-w-xl mx-auto py-6 px-4 bg-white rounded shadow-md text-center">
+    <h2 class="text-2xl font-bold mb-4">Thank You for Your Reservation!</h2>
+    <p class="mb-4">We have reserved a table for <strong>{partyName}</strong> on <strong>{date}</strong> at <strong>{time}</strong>.</p>
+    <p>Your reservation ID is <strong>{reservationId}</strong>. Please keep this ID for your records.</p>
+  </div>
+</section>
