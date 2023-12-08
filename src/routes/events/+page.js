1aa86a85cbc154cc09ba07d65164e9e05daf83d2
David Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-12-08 09:47:39 -0500

    Start events

---
+++ src/routes/events/+page.js
@@ -0,0 +1,9 @@
+const events = [
+  {
+    name: "Football Friday",
+    description:
+      "Every Friday, enjoy live football, tasty food, and a fun quiz with discounts at our Football.",
+    date: "EST",
+    time: "2:11 PM",
+  },
+];
