3599b0446759222bcaac6a20c22d143b996ba004
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-12-11 05:58:04 -0500

    Update Menu
    - Update images
    - Add modal

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,6 +1,19 @@
 <script>
+  import Modal from "$components/menu/Modal.svelte";
+  let isOpen = false;
+  let selectedItem = null;
+
   let menu;
   export { menu as data };
+
+  function openModal(item) {
+    selectedItem = item;
+    isOpen = true;
+  }
+
+  function closeModal() {
+    isOpen = false;
+  }
 </script>
 
 <div class="container mx-auto px-6 py-8">
@@ -15,9 +28,17 @@
             <img class="w-full h-40 mb-3 object-cover" src={item.img} alt={item.name} />
             <span class="text-lg font-bold">{item.name}</span>
             <span class="text-lg">{item.price}</span>
+            <button
+              on:click={() => openModal(item)}
+              class="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              >Open Modal</button
+            >
           </div>
         {/each}
       </div>
     </div>
   {/each}
+  {#if isOpen}
+    <Modal {isOpen} {selectedItem} on:close={closeModal} />
+  {/if}
 </div>
