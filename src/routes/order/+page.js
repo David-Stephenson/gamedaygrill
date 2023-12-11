0c5fa6738d7d6a0f2ee6a11880165727a383794b
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-12-11 17:03:08 -0500

    Fix navbar + add prettier

--- src/routes/order/+page.js
+++ src/routes/order/+page.js
@@ -1,9 +1,25 @@
 const menu = {
   appetizers: [
-    { name: "Grand Slam Sliders", price: "$7.99", img: "https://placehold.co/150" },
-    { name: "Knockout Nachos", price: "$6.99", img: "https://placehold.co/150" },
-    { name: "Curveball Coleslaw", price: "$6.99", img: "https://placehold.co/150" },
-    { name: "Kickball Kebabs", price: "$6.99", img: "https://placehold.co/150" },
+    {
+      name: "Grand Slam Sliders",
+      price: "$7.99",
+      img: "https://placehold.co/150",
+    },
+    {
+      name: "Knockout Nachos",
+      price: "$6.99",
+      img: "https://placehold.co/150",
+    },
+    {
+      name: "Curveball Coleslaw",
+      price: "$6.99",
+      img: "https://placehold.co/150",
+    },
+    {
+      name: "Kickball Kebabs",
+      price: "$6.99",
+      img: "https://placehold.co/150",
+    },
     { name: "Placerholder1", price: "$6.99", img: "https://placehold.co/150" },
   ],
   salads: [
@@ -28,24 +44,48 @@ const menu = {
     { name: "Placeholder12", price: "$4.99", img: "https://placehold.co/150" },
   ],
   kids: [
-    { name: "Chicken Nuggets", price: "$4.99", img: "https://placehold.co/150" },
+    {
+      name: "Chicken Nuggets",
+      price: "$4.99",
+      img: "https://placehold.co/150",
+    },
     { name: "Mini Pizza", price: "$5.99", img: "https://placehold.co/150" },
     { name: "Placeholder13", price: "$5.99", img: "https://placehold.co/150" },
     { name: "Placeholder14", price: "$5.99", img: "https://placehold.co/150" },
     { name: "Placeholder15", price: "$5.99", img: "https://placehold.co/150" },
   ],
   drinks: [
-    { name: "Coca Cola Products", price: "$1.99", img: "https://placehold.co/150" },
+    {
+      name: "Coca Cola Products",
+      price: "$1.99",
+      img: "https://placehold.co/150",
+    },
     { name: "Beer", price: "$1.99", img: "https://placehold.co/150" },
     { name: "Placeholder16", price: "$1.99", img: "https://placehold.co/150" },
     { name: "Placeholder17", price: "$1.99", img: "https://placehold.co/150" },
     { name: "Placeholder18", price: "$1.99", img: "https://placehold.co/150" },
   ],
   desserts: [
-    { name: "Slam Dunk Sundae", price: "$3.99", img: "https://placehold.co/150" },
-    { name: "Championship Cheesecake", price: "$4.99", img: "https://placehold.co/150" },
-    { name: "Home Run Brownie Sundae", price: "$3.99", img: "https://placehold.co/150" },
-    { name: "Overtime Oreo Cheesecake", price: "$4.99", img: "https://placehold.co/150" },
+    {
+      name: "Slam Dunk Sundae",
+      price: "$3.99",
+      img: "https://placehold.co/150",
+    },
+    {
+      name: "Championship Cheesecake",
+      price: "$4.99",
+      img: "https://placehold.co/150",
+    },
+    {
+      name: "Home Run Brownie Sundae",
+      price: "$3.99",
+      img: "https://placehold.co/150",
+    },
+    {
+      name: "Overtime Oreo Cheesecake",
+      price: "$4.99",
+      img: "https://placehold.co/150",
+    },
     { name: "Cheesecake", price: "$4.99", img: "https://placehold.co/150" },
   ],
 };
