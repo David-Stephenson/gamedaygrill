98f13679b10391be0da9e2b38ce53676deea1c0f
David Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-12-12 13:43:05 -0500

    Delete +page.js
    Not needed

--- src/routes/events/+page.js
+++
@@ -1,9 +0,0 @@
-const events = [
-  {
-    name: "Football Friday",
-    description:
-      "Every Friday, enjoy live football, tasty food, and a fun quiz with discounts at our Football.",
-    date: "EST",
-    time: "2:11 PM",
-  },
-];
