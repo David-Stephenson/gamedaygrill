cba577724e53117f75afcca8571f094b5675408d
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-12-11 21:16:10 -0500

    A lot of changes
    - Menu images
    - Remove order page
    - Start bag page
    
    Co-Authored-By: Yousif A <123918292+PharaohQueeto@users.noreply.github.com>

--- src/components/global/Navbar.svelte
+++ src/components/global/Navbar.svelte
@@ -1,30 +1,37 @@
 <script>
-  import { goto } from "$app/navigation";
-  import { information, contact } from "$config";
-  import { ShoppingBag, User2, MapPin, Phone } from "lucide-svelte";
+  import { information, contact } from '$config';
+  import { ShoppingBag, User2, MapPin, Phone } from 'lucide-svelte';
 
   // Links for the navbar
   const links = {
-    events: "/events",
-    menu: "/menu",
-    reserve: "/reserve",
-    order: "/order",
+    events: '/events',
+    menu: '/menu',
+    reserve: '/reserve',
   };
 </script>
 
 <div class="bg-white text-gray-800 px-8 py-3 shadow">
   <div class="flex justify-between items-center mb-3">
-    <a href="https://maps.app.goo.gl/CBtUFmniFkbwYs5q6" target="_blank" class="text-sm text-gray-600 flex items-center">
+    <a
+      href="https://maps.app.goo.gl/CBtUFmniFkbwYs5q6"
+      target="_blank"
+      class="text-sm text-gray-600 flex items-center"
+    >
       <MapPin size="16" class="mr-1" />
       {contact.address}
     </a>
-    <a href="tel:{contact.phone}" class="text-sm text-gray-600 flex items-center">
+    <a
+      href="tel:{contact.phone}"
+      class="text-sm text-gray-600 flex items-center"
+    >
       <Phone size="16" class="mr-1" />
       {contact.phone}
     </a>
   </div>
   <nav class="flex justify-between items-center">
-    <a href="/" class="text-2xl text-center text-red-600 font-russo">{information.name}</a>
+    <a href="/" class="text-2xl text-center text-red-600 font-russo"
+      >{information.name}</a
+    >
     <div class="flex justify-center flex-grow text-gray-800">
       {#each Object.entries(links) as [name, url]}
         <a href={url} class="text-base mx-4 capitalize">{name}</a>
---
+++ src/routes/bag/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+  import { bag } from '$lib/stores';
+  let bagContent;
+
+  bag.subscribe(value => {
+    bagContent = value;
+    console.log(bagContent);
+  });
+</script>
+
+<div class="container mx-auto px-4 sm:px-8">
+  <div class="py-8">
+    <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+      <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+        <table class="min-w-full leading-normal">
+          <thead>
+            <tr>
+              <th
+                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+              >
+                Product
+              </th>
+              <th
+                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+              >
+                Price
+              </th>
+              <th
+                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+              >
+                Comments
+              </th>
+              <th
+                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+              >
+                Quantity
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each bagContent as item (item.name)}
+              <tr>
+                <td
+                  class="px-5 py-5 border-b border-gray-200 bg-white text-base"
+                >
+                  <div class="flex items-center">
+                    <div class="flex-shrink-0 w-20 h-20">
+                      <img
+                        class="w-full h-full rounded-full"
+                        src={item.img}
+                        alt={item.name}
+                      />
+                    </div>
+                    <div class="ml-3">
+                      <p class="text-gray-900 whitespace-no-wrap">
+                        {item.name}
+                      </p>
+                    </div>
+                  </div>
+                </td>
+                <td
+                  class="px-5 py-5 border-b border-gray-200 bg-white text-base"
+                >
+                  <p class="text-gray-900 whitespace-no-wrap">{item.price}</p>
+                </td>
+                <td
+                  class="px-5 py-5 border-b border-gray-200 bg-white text-base"
+                >
+                  <p class="text-gray-900 whitespace-no-wrap">
+                    {item.comments}
+                  </p>
+                </td>
+                <td
+                  class="px-5 py-5 border-b border-gray-200 bg-white text-base"
+                >
+                  <p class="text-gray-900 whitespace-no-wrap">0</p>
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
