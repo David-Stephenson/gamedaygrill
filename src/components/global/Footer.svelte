190d25202d3309be5dddbb2544088682d4e4723a
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2024-02-16 17:46:54 -0500

    Update Footer.svelte
    Start to make footer look better

--- src/components/global/Footer.svelte
+++ src/components/global/Footer.svelte
@@ -10,30 +10,33 @@
   };
 </script>
 
-<footer class="bg-white text-gray-800 py-8 px-4">
-  <div
-    class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 items-start justify-center"
-  >
-    <div class="flex items-center space-x-2 md:space-x-4 justify-center">
-      <Logo class="h-12 fill-red-500" />
-      <div>
-        <h5 class="text-lg font-russo text-red-500 text-center">
-          {information.name}
-        </h5>
-        <p class="text-sm italic text-gray-600 text-center">
-          {information.slogan}
-        </p>
+<footer>
+  <div class="bg-gray-50 text-gray-800 py-8 px-4">
+    <div
+      class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 items-start justify-center"
+    >
+      <div class="flex items-center space-x-2 md:space-x-4 justify-center">
+        <Logo class="h-12 fill-red-500" />
+        <div>
+          <h5 class="text-lg font-russo text-red-500">
+            {information.name}
+          </h5>
+          <p class="text-sm italic text-gray-600">
+            {information.slogan}
+          </p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center text-gray-600 text-center">
+        <p class="text-sm">Mon-Fri 9am-9pm, Sat-Sun 10am-10pm</p>
+      </div>
+      <div class="flex space-x-2 items-center md:items-start justify-center">
+        {#each Object.entries(socialLinks) as [name, link]}
+          <a href={link.url} class="text-red-500 hover:text-red-700">
+            <svelte:component this={link.component} size={24} />
+          </a>
+        {/each}
       </div>
     </div>
-    <div class="flex flex-col items-center text-gray-600 text-center">
-      <p class="text-sm">Mon-Fri 9am-9pm, Sat-Sun 10am-10pm</p>
-    </div>
-    <div class="flex space-x-2 items-center md:items-start justify-center">
-      {#each Object.entries(socialLinks) as [name, link]}
-        <a href={link.url} class="text-red-500 hover:text-red-700">
-          <svelte:component this={link.component} size={24} />
-        </a>
-      {/each}
-    </div>
   </div>
+  <div class="h-2 w-full bg-red-500" />
 </footer>
