aee22dc5a29c9802f14c05071ccf4d2d8a23d502
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2024-02-18 21:59:32 -0500

    Update Config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/kit/vite";
-/** @type {import('@sveltejs/kit').Config} */
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
   kit: {
     adapter: adapter(),
     alias: {
-      $components: "./src/components",
-      $config: "./src/lib/config",
-      $stores: "./src/lib/stores",
+      $components: './src/components',
+      $config: './src/lib/config',
+      $stores: './src/lib/stores',
+      $lib: './src/lib',
     },
   },
-  preprocess: vitePreprocess(),
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 };
 export default config;
