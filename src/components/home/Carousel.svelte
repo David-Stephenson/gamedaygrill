1cdecfbe0e0f17a489b1c35866fe7a39222dde92
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2024-02-20 15:52:16 -0500

    Update Carousel.svelte

--- src/components/home/Carousel.svelte
+++ src/components/home/Carousel.svelte
@@ -4,7 +4,7 @@
   import '@splidejs/svelte-splide/css/core';
 </script>
 
-<div class="aspect-[16/3] rounded-[25px] overflow-hidden">
+<div class="h-96 rounded-[25px] overflow-hidden">
   <Splide
     aria-label="My Favorite Images"
     options={{
