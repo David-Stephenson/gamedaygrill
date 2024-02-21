e3870ef208bcd8dd6bf2f09550fbc6277e2442a4
David Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2024-02-21 18:46:11 -0500

    Update Hours.svelte

--- src/components/home/Hours.svelte
+++ src/components/home/Hours.svelte
@@ -1,84 +1,30 @@
-<div class="flex flex-col sm:content-center">
-  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="shadow overflow-hidden border-b border-red-500">
-        <table class="min-w-full divide-y divide-red-500">
-          <thead class="bg-red-500">
-            <tr>
-              <th
-                scope="col"
-                class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-              >
-                Day
-              </th>
-              <th
-                scope="col"
-                class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-              >
-                Hours
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-red-500">
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Monday</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-red-500">11am - 11pm</div>
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Tuesday</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-red-500">11am - 11pm</div>
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Wednesday</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-red-500">11am - 11pm</div>
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Thursday</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-red-500">11am - 11pm</div>
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Friday</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-red-500">11am - 1am</div>
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Saturday</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-red-500">11am - 1am</div>
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Sunday</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-red-500">11am - 10pm</div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+<script>
+  let hoursData = [
+    { day: 'Monday', hours: '11am - 11pm' },
+    { day: 'Tuesday', hours: '11am - 11pm' },
+    { day: 'Wednesday', hours: '11am - 11pm' },
+    { day: 'Thursday', hours: '11am - 11pm' },
+    { day: 'Friday', hours: '11am - 1am' },
+    { day: 'Saturday', hours: '11am - 1am' },
+    { day: 'Sunday', hours: '11am - 10pm' },
+  ];
+</script>
+
+<div class="flex flex-col h-full">
+  <div class="flex bg-red-500 text-white">
+    <div class="flex-1 p-2 text-center">Day</div>
+    <div class="flex-1 p-2 text-center">Hours</div>
+  </div>
+  <div class="flex flex-col flex-grow">
+    {#each hoursData as { day, hours }}
+      <div class="flex flex-grow border-t border-red-500">
+        <div class="flex-1 p-2 text-center flex items-center justify-center">
+          {day}
+        </div>
+        <div class="flex-1 p-2 text-center flex items-center justify-center">
+          {hours}
+        </div>
       </div>
-    </div>
+    {/each}
   </div>
 </div>
