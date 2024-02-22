0eda3d02e942ba01986813ebbd4f17c73d73d1f7
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2024-02-21 22:51:33 -0500

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,12 +1,12 @@
 // Restaurant Information
 export const information = {
-  name: "Game Day Grill",
-  slogan: "Come for the game, stay for the grill!",
+  name: 'Game Day Grill',
+  slogan: 'Come for the game, stay for the grill!',
   established: 2014,
 };
 
 // Restaurant Contact Information
 export const contact = {
-  phone: "(614) 764-4932",
-  address: "38 W Bridge St, Dublin, OH",
+  phone: '(614) 764-4932',
+  address: '4595 Bridge Park Ave, Dublin, OH',
 };
