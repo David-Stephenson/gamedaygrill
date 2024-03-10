0762e5b56a7a8fbdb17dab74fbf073413d044a17
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2024-03-10 13:57:15 -0400

    Update menu page
    Menu + Modal updates

---
+++ src/components/menu/Card.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  export let item;
+</script>
+
+<div
+  class="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+  on:click={() => dispatch('modalTrigger', item)}
+>
+  <div
+    class="bg-white shadow-md rounded-[25px] px-4 pb-4 flex flex-col items-center overflow-hidden cursor-pointer h-full border-2 border-red-500"
+  >
+    <picture class="w-full mb-3 object-cover drop-shadow">
+      <source type="image/avif" srcset="{item.image}.avif" />
+      <source type="image/webp" srcset="{item.image}.webp" />
+      <img
+        src="{item.image}.png"
+        alt={item.name}
+        class="w-full object-cover"
+        loading="lazy"
+      />
+    </picture>
+    <span class="text-lg font-bold">{item.name} • ${item.price}</span>
+    <span class="truncate-lines">{item.description}</span>
+  </div>
+</div>
+
+<style>
+  .truncate-lines {
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+</style>
--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,13 +1,88 @@
 <script>
-  import Modal from '$components/menu/NewModal.svelte';
+  import { onMount } from 'svelte';
+
+  import { fade, scale } from 'svelte/transition';
+
+  import { bag } from '$lib/stores.js';
+  import Card from '$components/menu/Card.svelte';
   import menu from '$lib/menu.js';
 
-  let isOpen = false;
-  let selectedItem = null;
+  import { createDialog, melt } from '@melt-ui/svelte';
+  import { X, Plus, Minus, ShoppingBag } from 'lucide-svelte';
 
-  function openModal(item) {
-    selectedItem = item;
-    isOpen = true;
+  import { nanoid } from 'nanoid';
+
+  const {
+    elements: { overlay, content, title, description, close, portalled },
+    states: { open },
+  } = createDialog({
+    forceVisible: true,
+  });
+
+  // Modal Logic
+  let selectedOptions = {};
+  let selectedItem;
+  let specialInstructions = '';
+  let quantity = 0;
+  let orderId;
+
+  function toggleOption(category, choice, selectMax) {
+    if (!selectedOptions[category]) {
+      selectedOptions[category] = {};
+    }
+
+    if (selectedOptions[category][choice]) {
+      delete selectedOptions[category][choice];
+    } else {
+      if (Object.keys(selectedOptions[category]).length < selectMax) {
+        selectedOptions[category][choice] = true;
+      } else {
+        console.log(
+          `Maximum of ${selectMax} selections allowed for ${category}.`,
+        );
+        return;
+      }
+    }
+
+    selectedOptions = { ...selectedOptions };
+  }
+
+  function updateBag() {
+    bag.update(items => {
+      const index = items.findIndex(item => item.orderId === orderId);
+      if (index === -1) {
+        if (quantity !== 0) {
+          items.push({
+            ...selectedItem,
+            selectedOptions,
+            specialInstructions,
+            quantity,
+            orderId,
+          });
+        }
+      } else {
+        if (quantity === 0) {
+          items.splice(index, 1);
+        } else {
+          items[index].quantity = quantity;
+        }
+      }
+      return items;
+    });
+  }
+
+  function openModal(event) {
+    selectedOptions = {};
+    quantity = 0;
+    selectedItem = event.detail;
+    orderId = nanoid(); // Generate a new unique ID for the order
+
+    if (event.detail.options) {
+      event.detail.options.forEach(option => {
+        selectedOptions[option.name] = {};
+      });
+    }
+    $open = true;
   }
 </script>
 
@@ -15,50 +90,158 @@
   <title>Menu | Game Day Grill</title>
 </svelte:head>
 
-<Modal bind:isOpen {selectedItem} />
-
+<!-- Menu Content -->
 <div class="container mx-auto px-6 py-8">
   {#each Object.keys(menu) as section (section)}
     <div class="mb-8">
-      <h2 class="text-2xl mb-3 capitalize font-russo">
+      <h2 class="text-2xl capitalize font-russo">
         {section}
       </h2>
       <div class="flex flex-wrap -mx-2 justify-center">
         <!-- Added justify-center here -->
         {#each menu[section] as item (item.name)}
-          <div
-            class="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
-            on:click={() => openModal(item)}
-          >
-            <div
-              class="bg-white shadow-md rounded-[25px] px-4 pb-4 flex flex-col items-center overflow-hidden cursor-pointer h-full border-2 border-red-500"
-            >
-              <picture class="w-full mb-3 object-cover drop-shadow">
-                <source type="image/avif" srcset="{item.image}.avif" />
-                <source type="image/webp" srcset="{item.image}.webp" />
-                <img
-                  src="{item.image}.png"
-                  alt={item.name}
-                  class="w-full object-cover"
-                  loading="lazy"
-                />
-              </picture>
-              <span class="text-lg font-bold">{item.name} • ${item.price}</span>
-              <span class="truncate-lines">{item.description}</span>
-            </div>
-          </div>
+          <Card {item} on:modalTrigger={openModal} />
         {/each}
       </div>
     </div>
   {/each}
 </div>
 
-<style>
-  .truncate-lines {
-    display: -webkit-box;
-    -webkit-line-clamp: 3; /* Number of lines you want to display */
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-</style>
+<!-- Modal -->
+<div class="" use:melt={$portalled}>
+  {#if $open}
+    <!-- Background -->
+    <div
+      use:melt={$overlay}
+      class="fixed inset-0 z-50 bg-black/50"
+      transition:fade={{ duration: 250 }}
+    />
+
+    <!-- Modal Content -->
+    <div
+      use:melt={$content}
+      class="fixed z-50 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]
+           w-full max-w-md md:max-w-2xl lg:max-w-4xl max-h-[85vh]
+           mx-auto p-6 md:p-6 space-y-4 md:space-y-6
+           bg-white rounded-[25px] shadow-xl
+           overflow-auto border-2 border-red-500"
+      transition:scale={{
+        duration: 250,
+        opacity: 0.0,
+        start: 0.8,
+      }}
+    >
+      <!-- Close button -->
+      <button
+        use:melt={$close}
+        aria-label="close"
+        class="absolute right-3 top-3 inline-flex
+                items-center justify-center rounded-full p-1 text-red-500
+                hover:bg-red-100 focus:shadow-red-500"
+      >
+        <X size="20" />
+      </button>
+
+      <!-- Top -->
+      <div class="">
+        <h2 class="text-xl md:text-2xl font-semibold" use:melt={$title}>
+          {selectedItem.name} • ${selectedItem.price}
+        </h2>
+        <p class="mt-2 text-sm md:text-base" use:melt={$description}>
+          {selectedItem.description}
+        </p>
+      </div>
+
+      <div class="flex flex-col md:flex-row gap-4">
+        <div class="md:flex-1">
+          <div class="srounded-[25px] overflow-hidden">
+            <picture class="aspect-w-1 aspect-h-1 w-full">
+              <source type="image/avif" srcset="{selectedItem.image}.avif" />
+              <source type="image/webp" srcset="{selectedItem.image}.webp" />
+              <img
+                src="{selectedItem.image}.png"
+                alt={selectedItem.name}
+                class="w-96 object-cover"
+                loading="lazy"
+              />
+            </picture>
+          </div>
+        </div>
+
+        <div class="md:flex-1 space-y-4">
+          {#if selectedItem.options}
+            <div class="space-y-2">
+              {#each selectedItem.options as option (option.name)}
+                <div>
+                  <h3 class="text-lg font-semibold">{option.name}</h3>
+                  <div class="grid grid-cols-2 md:grid-cols-3 gap-2">
+                    {#each option.choices as choice (choice)}
+                      <button
+                        class="relative inline-flex items-center justify-center py-2 px-4 rounded-lg transition-colors duration-150 cursor-pointer
+                        {selectedOptions[option.name][choice]
+                          ? 'bg-red-500 text-white'
+                          : 'bg-gray-100 text-gray-800 hover:bg-gray-200'}"
+                        on:click={() =>
+                          toggleOption(option.name, choice, option.selectMax)}
+                      >
+                        <span class="ml-2">{choice}</span>
+                      </button>
+                    {/each}
+                  </div>
+                </div>
+              {/each}
+            </div>
+          {/if}
+
+          <textarea
+            bind:value={specialInstructions}
+            class="w-full p-2 border border-gray-300 rounded-lg"
+            placeholder="Special instructions, notes, comments..."
+            rows="3"
+          ></textarea>
+
+          {#if quantity == 0}
+            <button
+              class="w-full bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-6 rounded-full transition-colors duration-150 flex items-center justify-center"
+              on:click={() => {
+                quantity = 1;
+                updateBag();
+              }}
+            >
+              <span class="mr-1"><ShoppingBag size="20" /></span> Add to Bag
+            </button>
+          {:else}
+            <div
+              class="w-full bg-red-500 text-white font-bold py-2 px-6 rounded-full transition-colors duration-150 flex items-center justify-between"
+            >
+              <button
+                class="focus:outline-none"
+                on:click={() => {
+                  if (quantity > 0) {
+                    quantity -= 1;
+                    updateBag();
+                  }
+                }}
+              >
+                <Minus size="24" />
+              </button>
+
+              <span class="mx-auto">
+                {quantity}
+              </span>
+              <button
+                class="focus:outline-none"
+                on:click={() => {
+                  quantity += 1;
+                  updateBag(); // Ensure this function handles quantity updates correctly
+                }}
+              >
+                <Plus size="24" />
+              </button>
+            </div>
+          {/if}
+        </div>
+      </div>
+    </div>
+  {/if}
+</div>
