7990ec71f81e6b8c6e445fca403e9d7125d2474f
David Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2024-03-24 16:58:13 -0400

    Update +page.svelte

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -46,151 +46,153 @@
   <title>Account | Game Day Grill</title>
 </svelte:head>
 
-<main class="max-w-5xl mx-auto">
-  <!-- User Info Card -->
-  <div
-    class="bg-white dark:bg-neutral-800 rounded-[25px] shadow-lg p-6 mb-8 flex items-center justify-between border-2 border-red-500"
-  >
-    <div class="flex items-center space-x-6">
-      <picture class="w-20 h-20 rounded-full">
-        <source type="image/avif" srcset="/images/account/profile.avif" />
-        <source type="image/webp" srcset="/images/account/profile.webp" />
-        <img
-          src="/images/account/profile.png"
-          alt="User Profile"
-          class="w-full h-full object-cover rounded-full"
-          loading="lazy"
-        />
-      </picture>
-      <div>
-        <div class="text-2xl font-bold dark:text-white">{user.name}</div>
-        <div class="text-gray-500 dark:text-gray-400">
-          MVP Since: {user.memberSince}
+<div class="flex-grow flex items-center justify-center">
+  <main class="max-w-5xl mx-auto">
+    <!-- User Info Card -->
+    <div
+      class="bg-white dark:bg-neutral-800 rounded-[25px] shadow-lg p-6 mb-4 flex items-center justify-between border-2 border-red-500"
+    >
+      <div class="flex items-center space-x-6">
+        <picture class="w-20 h-20 rounded-full">
+          <source type="image/avif" srcset="/images/account/profile.avif" />
+          <source type="image/webp" srcset="/images/account/profile.webp" />
+          <img
+            src="/images/account/profile.png"
+            alt="User Profile"
+            class="w-full h-full object-cover rounded-full"
+            loading="lazy"
+          />
+        </picture>
+        <div>
+          <div class="text-2xl font-bold dark:text-white">{user.name}</div>
+          <div class="text-gray-500 dark:text-gray-400">
+            MVP Since: {user.memberSince}
+          </div>
+        </div>
+      </div>
+      <div class="text-xl font-semibold dark:text-white">
+        {user.totalPoints} Points
+      </div>
+    </div>
+
+    <div class="grid gap-4 md:grid-cols-2">
+      <!-- Point Transaction History Card -->
+      <div
+        class="bg-white dark:bg-neutral-800 rounded-[25px] shadow-lg p-6 border-2 border-red-500"
+      >
+        <h3 class="text-xl text-center font-semibold mb-4 dark:text-white">
+          Point Transaction History
+        </h3>
+        <div class="overflow-auto">
+          <table class="min-w-full text-sm">
+            <thead>
+              <tr class="border-b dark:border-neutral-700">
+                <th
+                  class="px-4 py-2 text-left font-semibold text-left dark:text-white"
+                  >Date</th
+                >
+                <th
+                  class="px-4 py-2 text-left font-semibold text-left dark:text-white"
+                  >Points</th
+                >
+              </tr>
+            </thead>
+            <tbody>
+              {#each user.transactions as transaction}
+                <tr class="border-b dark:border-neutral-700">
+                  <td class="px-4 py-2 dark:text-white">{transaction.date}</td>
+                  <td class="px-4 py-2">
+                    <span
+                      class:text-green-500={transaction.points.startsWith('+')}
+                      class:text-red-500={!transaction.points.startsWith('+')}
+                    >
+                      {transaction.points}
+                    </span>
+                  </td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <!-- Upcoming Reservations Card -->
+      <div
+        class="bg-white dark:bg-neutral-800 rounded-[25px] shadow-lg p-6 border-2 border-red-500"
+      >
+        <h3 class="text-xl text-center font-semibold mb-4 dark:text-white">
+          Upcoming Reservations
+        </h3>
+        <div class="overflow-auto">
+          <table class="min-w-full text-sm">
+            <thead>
+              <tr class="border-b dark:border-neutral-700">
+                <th class="px-4 py-2 text-left font-semibold dark:text-white"
+                  >Party Name</th
+                >
+                <th class="px-4 py-2 text-left font-semibold dark:text-white"
+                  >Date</th
+                >
+                <th class="px-4 py-2 text-left font-semibold dark:text-white"
+                  >Time</th
+                >
+              </tr>
+            </thead>
+            <tbody>
+              {#each reservationDetails as reservation}
+                <tr class="border-b dark:border-neutral-700">
+                  <td class="px-4 py-2 dark:text-white"
+                    >{reservation.partyName}</td
+                  >
+                  <td class="px-4 py-2 dark:text-white">{reservation.date}</td>
+                  <td class="px-4 py-2 dark:text-white">{reservation.time}</td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <!-- Available Rewards Card -->
+      <div
+        class="bg-white dark:bg-neutral-800 rounded-[25px] shadow-lg p-6 border-2 border-red-500"
+      >
+        <h3 class="text-xl text-center font-semibold mb-4 dark:text-white">
+          Available Rewards
+        </h3>
+        <div class="overflow-auto">
+          <table class="min-w-full text-sm">
+            <thead>
+              <tr class="border-b dark:border-neutral-700">
+                <th class="px-4 py-2 text-left font-semibold dark:text-white"
+                  >Reward</th
+                >
+                <th class="px-4 py-2 text-left font-semibold dark:text-white"
+                  >Points Required</th
+                >
+                <th class="px-4 py-2 text-left font-semibold dark:text-white"
+                  >Expiration Date</th
+                >
+              </tr>
+            </thead>
+            <tbody>
+              {#each user.rewards as reward}
+                <tr class="border-b dark:border-neutral-700">
+                  <td class="px-4 py-2 text-left dark:text-white"
+                    >{reward.reward}</td
+                  >
+                  <td class="px-4 py-2 text-left dark:text-white"
+                    >{reward.pointsRequired}</td
+                  >
+                  <td class="px-4 py-2 text-left dark:text-white"
+                    >{reward.expirationDate}</td
+                  >
+                </tr>
+              {/each}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
-    <div class="text-xl font-semibold dark:text-white">
-      {user.totalPoints} Points
-    </div>
-  </div>
-
-  <div class="grid gap-8 md:grid-cols-2">
-    <!-- Point Transaction History Card -->
-    <div
-      class="bg-white dark:bg-neutral-800 rounded-[25px] shadow-lg p-6 border-2 border-red-500"
-    >
-      <h3 class="text-xl text-center font-semibold mb-4 dark:text-white">
-        Point Transaction History
-      </h3>
-      <div class="overflow-auto">
-        <table class="min-w-full text-sm">
-          <thead>
-            <tr class="border-b dark:border-neutral-700">
-              <th
-                class="px-4 py-2 text-left font-semibold text-left dark:text-white"
-                >Date</th
-              >
-              <th
-                class="px-4 py-2 text-left font-semibold text-left dark:text-white"
-                >Points</th
-              >
-            </tr>
-          </thead>
-          <tbody>
-            {#each user.transactions as transaction}
-              <tr class="border-b dark:border-neutral-700">
-                <td class="px-4 py-2 dark:text-white">{transaction.date}</td>
-                <td class="px-4 py-2">
-                  <span
-                    class:text-green-500={transaction.points.startsWith('+')}
-                    class:text-red-500={!transaction.points.startsWith('+')}
-                  >
-                    {transaction.points}
-                  </span>
-                </td>
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </div>
-
-    <!-- Upcoming Reservations Card -->
-    <div
-      class="bg-white dark:bg-neutral-800 rounded-[25px] shadow-lg p-6 border-2 border-red-500"
-    >
-      <h3 class="text-xl text-center font-semibold mb-4 dark:text-white">
-        Upcoming Reservations
-      </h3>
-      <div class="overflow-auto">
-        <table class="min-w-full text-sm">
-          <thead>
-            <tr class="border-b dark:border-neutral-700">
-              <th class="px-4 py-2 text-left font-semibold dark:text-white"
-                >Party Name</th
-              >
-              <th class="px-4 py-2 text-left font-semibold dark:text-white"
-                >Date</th
-              >
-              <th class="px-4 py-2 text-left font-semibold dark:text-white"
-                >Time</th
-              >
-            </tr>
-          </thead>
-          <tbody>
-            {#each reservationDetails as reservation}
-              <tr class="border-b dark:border-neutral-700">
-                <td class="px-4 py-2 dark:text-white"
-                  >{reservation.partyName}</td
-                >
-                <td class="px-4 py-2 dark:text-white">{reservation.date}</td>
-                <td class="px-4 py-2 dark:text-white">{reservation.time}</td>
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </div>
-
-    <!-- Available Rewards Card -->
-    <div
-      class="bg-white dark:bg-neutral-800 rounded-[25px] shadow-lg p-6 border-2 border-red-500"
-    >
-      <h3 class="text-xl text-center font-semibold mb-4 dark:text-white">
-        Available Rewards
-      </h3>
-      <div class="overflow-auto">
-        <table class="min-w-full text-sm">
-          <thead>
-            <tr class="border-b dark:border-neutral-700">
-              <th class="px-4 py-2 text-left font-semibold dark:text-white"
-                >Reward</th
-              >
-              <th class="px-4 py-2 text-left font-semibold dark:text-white"
-                >Points Required</th
-              >
-              <th class="px-4 py-2 text-left font-semibold dark:text-white"
-                >Expiration Date</th
-              >
-            </tr>
-          </thead>
-          <tbody>
-            {#each user.rewards as reward}
-              <tr class="border-b dark:border-neutral-700">
-                <td class="px-4 py-2 text-left dark:text-white"
-                  >{reward.reward}</td
-                >
-                <td class="px-4 py-2 text-left dark:text-white"
-                  >{reward.pointsRequired}</td
-                >
-                <td class="px-4 py-2 text-left dark:text-white"
-                  >{reward.expirationDate}</td
-                >
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-</main>
+  </main>
+</div>
